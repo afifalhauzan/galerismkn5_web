9ba07932f6664b81ec94cc792a970e630ea5fd45
afifalhauzan <afifalhauzan@student.ub.ac.id>
2025-12-05 20:43:53 +0700

    feat: Add ProgressProviders component and integrate @bprogress dependencies

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import SWRProvider from "@/providers/SWRProvider";
+import ProgressProviders from '@/app/bprogress';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SWRProvider>
-          <AuthProvider>
-            {children}
-          </AuthProvider>
-        </SWRProvider>
+        <ProgressProviders>
+          <SWRProvider>
+            <AuthProvider>
+              {children}
+            </AuthProvider>
+          </SWRProvider>
+        </ProgressProviders>
       </body>
     </html>
   );
